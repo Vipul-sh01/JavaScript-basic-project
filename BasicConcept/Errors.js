26a027587d3931bfb4a2d729c058089ef7398a95
Vipul sharma <vipulsharma6021@gmail.com>
2024-12-13 18:53:26 +0530

    add new projects

---
+++ BasicConcept/Errors.js
@@ -0,0 +1,38 @@
+// 1 ===  syntax error
+
+// function sum(a, b){
+//     let c = a+b;
+//     return c;
+
+
+// console.log(sum(2,5));
+
+// 2 === Reference Error
+
+// function sum(a, b){
+//     const c = a+b;
+//     return c;
+// }
+
+// console.log(sun(4, 7));
+
+// 3 === Logic Error
+
+// function sum(a,b){
+//     const c = a - b;
+//     return c;
+// }
+// console.log(sum(10, 20));
+
+// 4 === TypeError
+
+// const a = 45;
+// a.toUpperCase();
+
+// 5 === rangeError
+
+// const a = [];
+// a.length = -1;
+
+// 6 === costom Error
+// throw new Error("vipul");
