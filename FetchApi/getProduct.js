2f613437bfab7a430579e27f90079985b0172d25
Vipul sharma <vipulsharma6021@gmail.com>
2025-01-07 23:48:15 +0530

    Refactor getProduct.js to implement fetchPosts function for retrieving ranked posts from the Hive API

--- FetchApi/getProduct.js
+++ FetchApi/getProduct.js
@@ -1,14 +1,67 @@
-const url = 'https://bc62-103-226-203-170.ngrok-free.app/api/v1/books/getAllProduct';
+// const url = 'https://bc62-103-226-203-170.ngrok-free.app/api/v1/books/getAllProduct';
 
-const fetchData = async ()=>{
-    const response = await fetch(url);
-    const data = await response.json();
+// const fetchData = async ()=>{
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-    console.log(typeof data.data);
-    data.data.forEach((book) => console.log(book.title));
+//     console.log(typeof data.data);
+//     data.data.forEach((book) => console.log(book.title));
    
-    data.data.forEach((i)=> {
-       console.log(i.author);
+//     data.data.forEach((i)=> {
+//        console.log(i.author);
+//     });
+// }
+// fetchData();
+
+
+// const apiUrl = 'https://api.hive.blog';
+
+// const fetchData = async () => {
+//     const response = await fetch(apiUrl, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         jsonrpc: '2.0',
+//         method: 'bridge.get_discussion',
+//         params: {
+//           author: 'hiveio',
+//           permlink: 'around-the-hive-reflections',
+//           observer: 'alice',
+//         },
+//         id: 1,
+//       }),
+//     });
+//       const data = await response.json();
+//       console.log(typeof data); 
+// };
+
+// fetchData();
+
+const apiUrl = 'https://api.hive.blog';
+
+const fetchPosts = async () => {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'bridge.get_ranked_posts',  
+        params: {
+          sort: 'trending',  
+          tag: '',            
+          observer: 'alice',  
+        },
+        id: 1,
+      }),
     });
-}
-fetchData();
+    const data = await response.json();
+    console.log(data);
+    console.log(typeof data);
+    
+};
+
+fetchPosts();
