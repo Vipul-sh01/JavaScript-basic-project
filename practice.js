6f5215d40d93f8cb7681b8bc5ff3b728c9ee4ac9
Vipul sharma <vipulsharma6021@gmail.com>
2025-06-17 23:02:33 +0530

    Add practice.js file with sya function and age management example

---
+++ practice.js
@@ -0,0 +1,23 @@
+// function sya() {
+//     let age;
+
+//     return {
+//         setAge(newAge) {
+//             if (newAge >= 0) {
+//                 age = newAge;
+//             } else {
+//                 console.error("Age must be a non-negative number.");
+//             }
+//         },
+//         getAge() {
+//             console.log(age);
+//         }
+//     };
+// }
+
+
+// const person = sya();
+// person.setAge(25); 
+// person.getAge();   
+// person.setAge(-5); 
+
